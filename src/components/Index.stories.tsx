ffd0a076eb1879be71ed3596ded3806ab5bd0b24
Stanislav Gorovoy <cnfc19932@gmail.com>
2019-07-30 14:39:25 +0300

    Added prettier

--- src/components/Index.stories.tsx
+++ src/components/Index.stories.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Hello } from './Hello';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Hello } from "./Hello";
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Hello compiler={'Takoe'} framework={'React'} />
-  ))
-  .add('with emoji', () => (
-    <Hello compiler={'Takoe'} framework={'React'} />
-  ));   
+storiesOf("Button", module)
+    .add("with text", () => <Hello compiler={"Takoe"} framework={"React"} />)
+    .add("with emoji", () => <Hello compiler={"Takoe"} framework={"React"} />);
